8ff4422396a6a2abb960b90dc095f576258fbc67
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-11 10:33:21 -0400

    Updated code

--- src/Momentum.java
+++ src/Momentum.java
@@ -6,7 +6,17 @@ public class Momentum
     {
         Console c = new Console();
         
+        double mass, vel, mom;
+        
         c.print("Enter object's mass in kilograms: ");
+        mass = c.readDouble(); //collect input
+        
+        c.print("Enter object's velocity in meters per second: ");
+        vel = c.readDouble(); //collect input
+        
+        mom = mass * vel; //calculate momentum
+        
+        c.print("\n\nThe momentum of your object is " + mom); //output the answer
     }
     
 }
--- src/PizzaCost.java
+++ src/PizzaCost.java
@@ -4,7 +4,21 @@ public class PizzaCost
 {
     public static void main(String[] args) 
     {
+        Console c = new Console();
         
+        double labor = 1.50, util = .50, base = .50, top, topprice = .75, size, total;
+        
+        c.println("Welcome to Joey's Pizzera! What could we get started for you?");
+        
+        c.print("Size of your pizza (in inches): ");
+        size = c.readDouble();
+        
+        c.print("Number of toppings: ");
+        top = c.readDouble();
+        
+        total = labor + util + ((base * 2) * size) + (top * topprice);
+        
+        c.print("\nYour total is : " + total);
     }
     
 }
