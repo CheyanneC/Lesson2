740da373725c383fc11a172f4dc7a95b12a6e717
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-14 10:22:45 -0400

    Circle finished

--- src/BonusCircle.java
+++ src/BonusCircle.java
@@ -4,7 +4,18 @@ public class BonusCircle
 {
     public static void main(String[] args) 
     {
+        SketchPadWindow win = new SketchPadWindow(600, 600);
+        RainbowPen r = new RainbowPen(win);
         
+        r.up();
+        r.move(0, 100);
+        r.down();
+        
+        for(int x = 0; x < 100; x ++)
+        {
+            r.turn(3.6);
+            r.move(8);
+        }
     }
     
 }
