4df9c5b644fae9d5e2ad63a86bcebb595f1052ab
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-15 09:37:01 -0400

    Added comments to all programs

--- src/PizzaCost.java
+++ src/PizzaCost.java
@@ -6,19 +6,20 @@ public class PizzaCost
     {
         Console c = new Console();
         
-        double labor = 1.50, util = .50, base = .50, top, topprice = .75, size, total;
+        double labor = 1.50, util = .50, base = .50, top, topprice = .75, size, total; 
+        //allow for user to change cost easier
         
         c.println("Welcome to Joey's Pizzera! What could we get started for you?");
         
-        c.print("Size of your pizza (in inches): ");
+        c.print("Size of your pizza (in inches): "); //ask for size
         size = c.readDouble();
         
-        c.print("Number of toppings: ");
+        c.print("Number of toppings: "); //ask for # of toppings
         top = c.readDouble();
         
-        total = labor + util + ((base * 2) * size) + (top * topprice);
+        total = labor + util + ((base * 2) * size) + (top * topprice); //calculate total
         
-        c.print("\nYour total is : " + total);
+        c.print("\nYour total is : " + total); //output total
     }
     
 }
