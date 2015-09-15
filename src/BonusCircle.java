2dcd6e4d3ac68bba6efe93a774f94767b3c1a777
chey2540 <chey2540@ISS147D8-01.scdsb.pri>
2015-09-15 09:36:40 -0400

    Added comments to all programs

--- src/BonusCircle.java
+++ src/BonusCircle.java
@@ -8,13 +8,13 @@ public class BonusCircle
         RainbowPen r = new RainbowPen(win);
         
         r.up();
-        r.move(0, 100);
+        r.move(0, 100); //move to starting position
         r.down();
         
-        for(int x = 0; x < 100; x ++)
+        for(int x = 0; x < 100; x ++) //create circle
         {
-            r.turn(3.6);
-            r.move(8);
+            r.turn(3.6); //always rotate 3.6 to form the circle
+            r.move(8); //move 8 each time
         }
     }
     
